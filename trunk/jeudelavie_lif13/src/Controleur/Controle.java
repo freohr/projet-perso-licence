6f3a978834490c1d54d323742663b77baf557b8b
s7if.freohr@gmail.com <s7if.freohr@gmail.com@46f19874-f3f8-9be2-623b-17f7d2eb542d>
2013-12-04 15:30:31 +0000

    ajout controleur séparé de la vue init monde et pause monde fonctionnent

---
+++ trunk/jeudelavie_lif13/src/Controleur/Controle.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+
+import modele.Monde;
+
+/**
+ *
+ * @author p1006099
+ */
+public class Controle {
+
+    protected Monde monde;
+
+    public Controle() {
+    }
+
+    public Controle(Monde monde) {
+        this.monde = monde;
+    }
+
+    public Monde getMonde() {
+        return monde;
+    }
+    
+    public void setModele(Monde monde) {
+        this.monde = monde;
+    }
+
+    public void initMonde(int size) {
+        if (size > 0) {
+            monde.init(size);
+        }
+    }
+
+    public void pause() {
+        if (monde.isPaused()) {
+            monde.resume();
+        } else {
+            monde.pause();
+        }
+
+    }
+    
+    public void stop() {
+        if(monde.isStopped()) {
+            monde.random();
+            monde.run();
+        } else {
+            monde.stop();
+        }
+    }
+}
